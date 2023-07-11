dc4f9a335935f292ebef4260957d2367db76d5ad
Alik Agarwala <alikagarwala@outlook.com>
2023-07-11 14:29:13 +0530

    Add comments in the script

--- enabler.sh
+++ enabler.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+# Update the package database and upgrade the system
 sudo pacman -Syyu
+
+# Install BlueZ and BlueZ utilities
 sudo pacman -S bluez bluez-utils
+
+# Start the Bluetooth service
 sudo systemctl start bluetooth.service
+
+# Enable the Bluetooth service to start on boot
 sudo systemctl enable bluetooth.service
